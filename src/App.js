6e05a9c302efbd17ca0f50e500aa959c5206ebbc
Mi Sun Choi <misunchoi@mis-macbook-air.attlocal.net>
2020-02-11 21:34:47 -0800

    basic home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Resume</Nav.Link>
+          <Nav.Link href="#pricing">Contact</Nav.Link>
+        </Nav>
+      </Navbar>
+
+      <footer className="footer">Copyright 2020</footer>
     </div>
   );
 }
