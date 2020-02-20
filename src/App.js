829403a41786d81692101a22c77d209bc03e7969
Mi Sun Choi <misunchoi@mis-macbook-air.attlocal.net>
2020-02-19 21:48:25 -0800

    add react-router-dom and pages

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import React from "react";
 import "./App.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import CarouselComponent from "./components/Carousel"
+import { Route, Link } from 'react-router-dom';
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="background">
       <Navbar bg="light" variant="light">
-        <Navbar.Brand href="#home">Michelle Choi</Navbar.Brand>
+        <Link to="/"><Navbar.Brand>Michelle Choi</Navbar.Brand></Link>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#resume">Resume</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link><Link to="/projects">Projects</Link></Nav.Link>
+          <Nav.Link><Link to="/resume">Resume</Link></Nav.Link>
+          <Nav.Link><Link to="/contact">Contact</Link></Nav.Link>
         </Nav>
       </Navbar>
 
-      <CarouselComponent />
+      <Route path="/" component={Home} exact/>
+      <Route path="/projects" component={Projects} exact/>
+      <Route path="/resume" component={Resume} exact/>
+      <Route path="/contact" component={Contact} exact/>
 
       <footer className="footer">Copyright 2020</footer>
     </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CarouselComponent from "../components/Carousel";
+
+function Home() {
+  return (
+    <div>
+      <CarouselComponent />
+    </div>
+  );
+}
+
+export default Home;
