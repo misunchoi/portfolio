86ddb231940531325e3d8c7d362045cb9eafd1d0
Mi Sun Choi <misunchoi@Mis-MacBook-Air.attlocal.net>
2020-06-14 23:24:41 -0700

    update contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,123 +1,37 @@
 import React from "react";
-import contactBackground from "../images/contact-background.jpg";
-import contactMobileBackground from "../images/contact-mobile-background.jpg";
+import contactBackground from "../images/contact-image.png";
 import styled from "styled-components";
-import email from "../images/email.svg";
-import phone from "../images/phone.svg";
-import linkedIn from "../images/linkedIn.svg";
+import Image from "../components/LoadImage";
 
-const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-  font-size: 24px;
-`;
-
-const BackgroundImage = styled.div`
-  height: 100%;
-  background-image: url(${contactBackground});
-  background-repeat: no-repeat;
-  background-size: cover;
-
-  @media only screen and (max-width: 1024px) {
-    background-image: url(${contactMobileBackground});
-  }
-`;
-
-const MobileContainer = styled.div`
-  @media only screen and (min-width: 1024px) {
-    display: none;
-  }
+const RowWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   margin: auto;
-  width: 80%;
-  max-width: 300px;
-  position: absolute;
-  left: 50%;
-  top: 0;
-  transform: translate(-50%, 84px);
+  width: fit-content;
 `;
 
-const Container = styled.div`
-  @media only screen and (max-width: 1023px) {
-    display: none;
-  }
-  display: flex;
-  flex-direction: column;
-  margin: auto;
-  max-width: 1200px;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
+const Description = styled.div`
+  background-color: #dcdbdb;
+  margin: 1em 1em 1em 4em;
+  padding: 3em;
+  width: 468px;
 `;
 
-const Title = styled.td`
-  text-align: right;
-
-  @media only screen and (max-width: 1024px) {
-    text-align: center;
-    font-size: 24px;
-    font-weight: 500;
-    margin-top: 1em;
-  }
-`;
-
-const Content = styled.a`
-  font-size: inherit;
-  text-align: center;
-  color: inherit;
-
-  @media only screen and (max-width: 1024px) {
-    font-size: 18px;
-  }
-`;
-
-const Icon = styled.td`
-  padding: 0.5em 1em;
-`;
-
-function Contact() {
+const Contact = () => {
   return (
-    <Wrapper>
-      <BackgroundImage />
-      <MobileContainer>
-        <Title>Email</Title>
-        <Content href="mailto:choi.msc033@gmail.com">choi.msc033@gmail.com</Content>
-
-        <Title>Phone</Title>
-        <Content href="tel:1-619-880-0527">(619) 880-0527</Content>
-
-        <Title>LinkedIn</Title>
-        <Content href="https://linkedin.com/in/michelle-misun-choi" rel="noopener noreferrer" target="_blank">linkedin.com/in/michelle-misun-choi</Content>
-      </MobileContainer>
-
-      <Container>
-        <table>
-          <tr>
-            <Title>Email</Title>
-            <Icon>
-              <img src={email} width={55} />
-            </Icon>
-            <td><Content href="mailto:choi.msc033@gmail.com">choi.msc033@gmail.com</Content></td>
-          </tr>
-          <tr>
-            <Title>Phone</Title>
-            <Icon>
-              <img src={phone} width={55} />
-            </Icon>
-            <td>‪<Content href="tel:1-619-880-0527">(619) 880-0527</Content></td>
-          </tr>
-          <tr>
-            <Title>LinkedIn</Title>
-            <Icon>
-              <img src={linkedIn} width={55} />
-            </Icon>
-            <td><Content href="https://linkedin.com/in/michelle-misun-choi" rel="noopener noreferrer" target="_blank">linkedin.com/in/michelle-misun-choi</Content></td>
-          </tr>
-        </table>
-      </Container>
-    </Wrapper>
+      <RowWrapper>
+        <Image src={contactBackground} thumb={contactBackground} alt="Profile picture" width={468}/>
+        <Description>
+          <h1>Email</h1>
+          <a href="mailto:choi.msc033@gmail.com">choi.msc033@gmail.com</a>
+          <br/>
+          <h1>Phone</h1>
+          <a href="tel:1-619-880-0527">(619) 880-0527</a>
+          <br/>
+          <h1>LinkedIn</h1>
+          <a href="https://linkedin.com/in/michelle-misun-choi" rel="noopener noreferrer" target="_blank">linkedin.com/in/michelle-misun-choi</a>
+        </Description>
+      </RowWrapper>
   );
 }
 
